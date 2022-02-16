f4c34dc61a22b2c36a07b6900ee4af837587d37c
Azay Genius <azay.genius8@gmail.com>
2022-02-16 10:51:25 +0000

    Header:navbar

--- Clone 1/ecomm/src/components/Navbar.jsx
+++ Clone 1/ecomm/src/components/Navbar.jsx
@@ -1,11 +1,77 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import food from "../assets/food.png";
 
 function Navbar() {
   return (
-    <div>
-        Nav area
-    </div>
-  )
+    <Nav>
+      <div className="brand">
+        <img src={food} alt="food logo" width="32px" height='28' />
+        <div className="toggle"></div>
+      </div>
+      <ul className="links">
+        <a href="#home">
+          <li className="active">Home</li>
+        </a>
+        <a href="#our services">
+          <li>Services</li>
+        </a>
+        <a href="#portfolio">
+          <li>Portfolio</li>
+        </a>
+        <a href="#testimonial">
+          <li>Testimonial</li>
+        </a>
+        <a href="#products">
+          <li>Products</li>
+        </a>
+        <a href="#newsletter">
+          <li>Newsletter</li>
+        </a>
+      </ul>
+
+    </Nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6px 4vw;
+
+  .brand {
+    img {
+      cursor: pointer;
+      margin-top: 0.5rem;
+    }
+  .toggle {
+    display: none;
+  }
+  }
+
+  .links {
+    display: flex;
+    gap: 2rem;
+    list-style: none;
+      a {
+        color: #0A2463;
+        font-weight: 400;
+        text-decoration: none;
+        text-transform: uppercase;
+        letter-spacing:0.2rem;
+        transition: 0.1s ease-in-out;
+
+        &:hover{
+            color:#FFB997;
+        }
+      }
+    
+          .active{
+              color:#FFB997;
+          }
+    
+  }
+`;
