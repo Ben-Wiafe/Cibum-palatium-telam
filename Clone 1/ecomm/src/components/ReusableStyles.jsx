6bd65a156c67228475e673f17b2895f840edde5c
Bernard <70757557+Ben-Wiafe@users.noreply.github.com>
2022-02-17 10:26:22 +0000

    Reusable styles

---
+++ Clone 1/ecomm/src/components/ReusableStyles.jsx
@@ -0,0 +1,50 @@
+import {css} from 'styled-components';
+export const TitleSytles = css`
+    .title{
+        text-align: center;
+        display:flex;
+        flex-direction: column;
+        gap:2rem;
+
+        h1{
+            font-size:3rem;
+            text-transform: uppercase;
+
+            span{
+                color:#FFB997;
+            }
+        }
+        .yellow{
+            color:#FFB997;
+        }
+        p{
+            padding:0 10vw;
+            font-size:1.1rem;
+            line-height:2rem;
+            letter-spacing:0.1rem;
+
+        }
+    }
+    @media screen and (min-width:260px )and (max-width:1080px){
+        .title {
+            gap:1rem;
+            
+            h1{
+                font-size: 1.4rem;
+            }
+            p{
+                padding:0 1vw;
+            }
+        }
+    }
+
+`;
+
+export const imageZoomEffect = css`
+    img{
+        transition:0.8s ease-in-out;
+        &:hover{
+            transform: scale(1.2)
+        }
+    }
+`
